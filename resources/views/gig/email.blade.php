4537afa8d6d672f4a6703b0195d8b45ce7cbe082
Andrew Haswell <andrew.haswell@visualsoft.co.uk>
2017-06-14 20:00:46 +0100

    Changs to email template

--- resources/views/gig/email.blade.php
+++ resources/views/gig/email.blade.php
@@ -1,19 +1,8 @@
 <html>
 <head>
   <title>Email Update</title>
-  <style>
-    body {
-      background-image: url("{{ URL::asset('images/graffiti_background.jpg') }}");
-      font-family:      Roboto, fantasy;
-      }
-
-    h1, h2, h3 {
-      font-family: Captain;
-      font-size:   40pt;
-      }
-  </style>
 </head>
-<body>
+<body background="{{ URL::asset('images/graffiti_background.jpg') }}">
 <img src="{{ URL::asset('images/insangel.png') }}"/>
 <h3>Gigs Approved!</h3>
 <p>Thanks for adding your gigs to Insangel.co.uk - they've been approved and are now live on the site.</p>
