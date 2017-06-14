65642091b34f293a689ab5db53d3268ecce0edc9
Andrew Haswell <andrew.haswell@visualsoft.co.uk>
2017-06-14 20:06:41 +0100

    Changes to email template

--- resources/views/gig/email.blade.php
+++ resources/views/gig/email.blade.php
@@ -2,8 +2,8 @@
 <head>
   <title>Email Update</title>
 </head>
-<body background="{{ URL::asset('images/graffiti_background.jpg') }}">
-<img src="{{ URL::asset('images/insangel.png') }}"/>
+<body>
+<img src="{{ URL::asset('images/insangel.png') }}" width="100"/>
 <h3>Gigs Approved!</h3>
 <p>Thanks for adding your gigs to Insangel.co.uk - they've been approved and are now live on the site.</p>
 <a href="{{URL::to('/other')}}">North East Gig Guide</a>
